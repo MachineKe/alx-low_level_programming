68eefa1caa28effd8e61d0c89a1dcbd4308c8840
Machineke <markarapsoi72@gmail.com>
2022-12-29 00:16:48 +0300

    3

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -6,13 +6,13 @@
  */
 int main(void)
 {
-	char c;
-	c = 'a';
-	while (c <= 'z') 
-	{
-			putchar(c);
-			c++;
-	}
+char c;
+c = 'a';
+while (c <= 'z') 
+{
+putchar(c);
+c++;
+}
 putchar('\n');
 return (0);
 }
